02f4b48356ef49299cfc742e9c3c43e76e7757b2
luthfifaqih <luthfifaqih1122@gmail.com>
2024-05-03 19:01:54 +0700

    init commit modul users

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+
+@section('dashboard')
+    <div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+        <div class="post d-flex flex-column-fluid" id="kt_post">
+            <div id="kt_content_container" class="container-xxl">
+
+
+
+
+
+            </div>
+        </div>
+    </div>
+@endsection
